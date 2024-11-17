b615e3f616cd215fd366b2344bda078a4586470f
Fornicis <blizzard_91@hotmail.co.uk>
2024-11-17 07:43:00 +0000

    Added varaint names to seperate file to solve recursive imports

---
+++ game_config.py
@@ -0,0 +1,5 @@
+# Just the variant names, used for kill tracking
+VARIANT_TYPES = {
+    "Frenzied", "Ancient", "Ethereal", "Colossal", "Corrupted", 
+    "Swift", "Vampiric", "Armoured", "Void-Touched"
+}
